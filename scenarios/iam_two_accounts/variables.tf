3d5959ee0bfbf5408a441ad251503c5969a0b8a1
andrewnixdorf <andrew.nixdorf@clariondoor.com>
2023-01-15 03:55:14 -0800

    Getting Started

--- scenarios/iam_two_accounts/variables.tf
+++ scenarios/iam_two_accounts/variables.tf
@@ -2,13 +2,13 @@ variable "iam_users" {
   type        = map(map(list(string)))
   description = "A list of users you want to create inside the \"users\" account"
   default = {
-    admin = {
+    SorryNotSorry = {
       groups = ["admins"]
     }
-    user = {
+    SorryNotSorryDev = {
       groups = ["users"]
     }
-    service_user = {
+    SorryNotSorry4Partying = {
       groups = ["service_users"]
     }
   }
